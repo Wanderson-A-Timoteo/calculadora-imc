0b69ba45c69dcaa88fda8184ae3e93897aea5163
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-01-31 18:04:17 -0400

    Limpado, organizado o projeto, e iniciado a estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMG</span>
+
+      <div className="area-input">
+        <input type="text" placeholder="Peso em (Kg) Ex: 90" />
+
+        <input type="text" placeholder="Altura em (Cm) Ex: 180" />
+
+        <button>Calcular</button>
+      </div>
+      <h2>Seu IMC foi 25, você esta abaixo do peso!</h2>
     </div>
   );
 }
-
-export default App;
